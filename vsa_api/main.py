83f6e4c61236df959d5a69dbb741c332928a7059
NamVo <nhokmant2003@gmail.com>
2024-05-18 19:59:22 +0700

    fix code & add more

--- vsa_api/main.py
+++ vsa_api/main.py
@@ -1,17 +1,26 @@
 from st_pages import Page, show_pages
 import streamlit as st
 
-
-# Specify what pages should be shown in the sidebar, and what their titles 
+# Specify what pages should be shown in the sidebar, and what their titles
 # and icons should be
 show_pages(
     [
-        Page("../Sentiment_Analysis_Vietnamese/vsa_ap/main.py", "Introduction", ":brain:"),
-        Page("../Sentiment_Analysis_Vietnamese/vsa_ap/pages/app.py", "Application", ":video_game:"),
-        Page("../Sentiment_Analysis_Vietnamese/vsa_ap/pages/members.py", "Member", ":adult:")
+        Page("../Sentiment_Analysis_Vietnamese/vsa_api/main.py", "Introduction", ":brain:"),
+        Page("../Sentiment_Analysis_Vietnamese/vsa_api/pages/members.py", "Member", ":adult:"),
+        Page("../Sentiment_Analysis_Vietnamese/vsa_api/pages/app.py", "Application", ":video_game:"),
     ]
 )
 
 st.write("""
 # :brain: Giới thiệu chung
+Trong những năm gần đây, phân tích và nhận diện cảm xúc ngày càng trở nên phổ biến để xử lý dữ liệu truyền thông xã hội trên các cộng đồng trực tuyến, blog, wiki, nền tảng tiểu blog và các phương tiện cộng tác trực tuyến khác. Phân tích nhận diện cảm xúc là một nhánh của nghiên cứu điện toán sinh thái nhằm phân loại văn bản (nhưng đôi khi cả âm thanh và video ) thành tích cực hoặc tiêu cực.
+
+Đây là một lĩnh vực liên quan đến truy xuất thông tin và tổng hợp thông tin vì nó yêu cầu dữ liệu phải được thu thập, tích hợp và phân loại. Hầu hết các tài liệu về ngôn ngữ tiếng Anh nhưng gần đây ngày càng có nhiều ấn phẩm đề cập đến vấn đề đa ngôn ngữ. Hệ thống phân tích nhận diện cảm xúc có thể được phân loại rộng rãi thành dựa trên tri thức và dựa trên thống kê. Trong khi hầu hết các công việc áp dụng nó như là một bài toán phân loại đơn giản, phân tích cảm xúc là một bài toán nghiên cứu đòi hỏi phải giải quyết nhiều nhiệm vụ NLP (Natural Language Processing), bao gồm nhận dạng thực thể được đặt tên, trích xuất khái niệm, phát hiện châm biếm, trích xuất khía cạnh và phát hiện tính chủ quan. Phát hiện tính chủ quan là một nhiệm vụ cần thiết của phân tích cảm xúc vì hầu hết các công cụ phát hiện cảm tính đều được tối ưu hóa để phân biệt giữa văn bản tích cực và tiêu cực
+
+Hiện tại thì cộng đồng khoa học mới chỉ giải quyết tốt bài toán phân tích và nhận diện cảm xúc trong văn bản tiếng Việt ở cấp độ đơn giản, tức là phân tích cảm xúc với hai lớp cảm xúc tiêu cực và tích cực với độ chính xác hơn 85%. Bài toán phân tích cảm xúc có một số phương pháp giải quyết như sau:
+
+- Phương pháp thủ công (dò từ khóa): việc dự đoán cảm xúc dựa vào việc tìm kiếm các từ cảm xúc riêng lẻ, xác định điểm số cho các từ tích cực, xác định điểm số cho các từ tiêu cực và sau đó là tổng hợp các điểm số này lại theo một độ đo xác định để quyết định xem văn bản mang màu sắc cảm xúc gì. Điểm hạn chế của phương pháp này là quan tâm đến thứ tự các từ và sẽ bỏ qua các từ quan trọng. Độ chính xác của mô hình phụ thuộc vào độ tốt của bộ từ điển các từ cảm xúc.Ưu điểm của phương pháp này là dễ thực hiện, tính toán nhanh, chỉ tốn công sức cho việc xây dựng bộ từ điển dữ liệu của các từ cảm xúc thôi.
+- Phương pháp Deep Learning Neural Network: phương pháp phân tích nhận diện cảm xúc đã được giải quyết bằng mô hình học Recurrent Neural Network với một phương pháp được dùng phổ biến hiện nay là Long Short Term Memory Neural Network (LSTMs), kết hợp với phương pháp mô hình vector hóa từ Word2Vector với kiến trúc được sử dụng là Continuous Bag- of-Words (CBOW).
+- Phương pháp kết hợp rule-based và corpus-based: Phương pháp này kết hợp sử dụng mô hình Deep Learning Recursive Neural Network với hệ tri thức chuyên gia được sử dụng trong xử lý ngôn ngữ tự nhiên được gọi là Sentiment Treebank. Sentiment Tree là một mô hình cây phân tích cú pháp của một câu văn, trong đó ở mỗi nút trong cây được kèm theo bộ trọng số cảm xúc lần lượt là: rất tiêu cực, tiêu cực, trung tính, tích cực và rất tích cực.
+
 """)
--- vsa_api/pages/members.py
+++ vsa_api/pages/members.py
@@ -1,9 +1,41 @@
 import streamlit as st
 
 st.set_page_config(
-        page_title="Members of Team"
+    page_title="Members of Team"
 )
 
 st.write("""
 # :adult: Thành viên
+
+### Nhóm gồm hai thành viên:
+""")
+
+col1, col2 = st.columns([1, 1])
+
+with col1:
+    st.image("../Sentiment_Analysis_Vietnamese/vsa_api/images/Duc1.jpg", caption="Huỳnh Trọng Đức - 21IT129")
+    st.write("""
+    - Vai trò: Member
+    - Họ tên: Huỳnh Trọng Đức
+    - Mã sinh viên: 21IT129
+    - Lớp sinh hoạt: 21SE3
+    - Khoa: Khoa Học Máy Tính
+    - Ngành: Công Nghệ Thông Tin
+    """)
+
+with col2:
+    st.image("../Sentiment_Analysis_Vietnamese/vsa_api/images/Nam.jpg", caption="Võ Văn Nam - 21IT683")
+    st.write("""
+    - Vai trò: Leader
+    - Họ tên: Võ Văn Nam
+    - Mã sinh viên: 21IT683
+    - Lớp sinh hoạt: 21SE3
+    - Khoa: Khoa Học Máy Tính
+    - Ngành: Công Nghệ Thông Tin
+    """)
+
+st.write("""
+### Github: [Github Of Application](https://github.com/NamVo181203/Sentiment_Analysis_Vietnamese)
+
+### Driver: [Driver Of Model](https://drive.google.com/drive/folders/1-KxZ3F_8OGifimZXUuQkeL7ZDSyUnvN5?usp=sharing)
 """)
